2bd3d99df92d318ec7f79275e1bec2e3b9d95286
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2020-09-16 12:51:08 +0600

    Ooh, aah, la la la

---
+++ admin/dashboard.php
@@ -0,0 +1,133 @@
+                                                <h1 class="text-primary">
+													<i class="fas fa-tachometer-alt"></i> Dashboard 
+													<small>Statistics Overview</small>
+												</h1>
+												<ol class="breadcrumb">
+													<li class="active">
+														<i class="fas fa-tachometer-alt"></i> Dashboard
+													</li>
+												</ol>
+												<div class="row">
+													<div class="col-sm-4">
+														<div class="panel panel-primary">
+															<div class="panel-heading">
+																<div class="row">
+																	<div class="col-xs-3">
+																		<i class="fa fa-users fa-5x"></i>
+																	</div>
+																	<div class="col-xs-9">
+																		<div class="pull-right" style="font-size: 45px">10</div>
+																		<div class="clearfix"></div>
+																		<div class="pull-right">All Students</div>
+																	</div>
+																</div>
+															</div>
+															<a href="">
+																<div class="panel-footer">
+																	<span>All Students</span>
+																	<span class="pull-right">
+																		<i class="fas fa-arrow-circle-right"></i>
+																	</span>
+																	<div class="clearfix"></div>
+																</div>
+															</a>
+														</div>
+													</div>
+													<div class="col-sm-4">
+														<div class="panel panel-primary">
+															<div class="panel-heading">
+																<div class="row">
+																	<div class="col-xs-3">
+																		<i class="fa fa-users fa-5x"></i>
+																	</div>
+																	<div class="col-xs-9">
+																		<div class="pull-right" style="font-size: 45px">10</div>
+																		<div class="clearfix"></div>
+																		<div class="pull-right">All Students</div>
+																	</div>
+																</div>
+															</div>
+															<a href="">
+																<div class="panel-footer">
+																	<span>All Students</span>
+																	<span class="pull-right">
+																		<i class="fas fa-arrow-circle-right"></i>
+																	</span>
+																	<div class="clearfix"></div>
+																</div>
+															</a>
+														</div>
+													</div>
+													<div class="col-sm-4">
+														<div class="panel panel-primary">
+															<div class="panel-heading">
+																<div class="row">
+																	<div class="col-xs-3">
+																		<i class="fa fa-users fa-5x"></i>
+																	</div>
+																	<div class="col-xs-9">
+																		<div class="pull-right" style="font-size: 45px">10</div>
+																		<div class="clearfix"></div>
+																		<div class="pull-right">All Students</div>
+																	</div>
+																</div>
+															</div>
+															<a href="">
+																<div class="panel-footer">
+																	<span>All Students</span>
+																	<span class="pull-right">
+																		<i class="fas fa-arrow-circle-right"></i>
+																	</span>
+																	<div class="clearfix"></div>
+																</div>
+															</a>
+														</div>
+													</div>
+												</div>
+
+                                               <hr>
+
+											   <h3>New Students</h3>
+
+											   <div class="table-responsive">
+
+											   <table id="data" class="table table-hover table-bordered table-striped">
+											      <thead>
+												     <tr>
+													 <th>ID</th>
+													 <th>Name</th>
+													 <th>Roll</th>
+													 <th>City</th>
+													 <th>Contact</th>
+													 <th>Photo</th>
+													 </tr>
+												  </thead>
+												  <tbody>
+												    <tr>
+												       <td>1</td>
+													   <td>Faizul Islam</td>
+													   <td>1810021</td>
+													   <td>Rajshahi</td>
+													   <td>01715577668</td>
+													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
+												    </tr>
+													<tr>
+												       <td>1</td>
+													   <td>Faizul Islam</td>
+													   <td>1810021</td>
+													   <td>Rajshahi</td>
+													   <td>01715577668</td>
+													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
+												    </tr>
+													<tr>
+												       <td>1</td>
+													   <td>Faizul Islam</td>
+													   <td>1810021</td>
+													   <td>Rajshahi</td>
+													   <td>01715577668</td>
+													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
+												    </tr>
+												  </tbody>
+											   </table>
+
+											  </div>
--- admin/index.php
+++ admin/index.php
@@ -76,16 +76,16 @@ if(!isset($_SESSION['user_login'])){
 
 										<div class="col-sm-3">
 											<div class="list-group">
-												<a href="#" class="list-group-item active">
+												<a href="index.php?page=dashboard" class="list-group-item active">
 													<i class="fas fa-tachometer-alt"></i> Dashboard
 												</a>
-												<a href="#" class="list-group-item">
+												<a href="index.php?page=add-student" class="list-group-item">
 													<i class="fa fa-user-plus"></i> Add Student
 												</a>
-												<a href="#" class="list-group-item">
+												<a href="index.php?page=all-students" class="list-group-item">
 													<i class="fa fa-users"></i> All Students
 												</a>
-												<a href="#" class="list-group-item">
+												<a href="index.php?page=all-users" class="list-group-item">
 													<i class="fa fa-users"></i> All Users
 												</a>
 											</div>
@@ -95,140 +95,28 @@ if(!isset($_SESSION['user_login'])){
 
 										<div class="col-sm-9">
 											<div class="content">
-												<h1 class="text-primary">
-													<i class="fas fa-tachometer-alt"></i> Dashboard 
-													<small>Statistics Overview</small>
-												</h1>
-												<ol class="breadcrumb">
-													<li class="active">
-														<i class="fas fa-tachometer-alt"></i> Dashboard
-													</li>
-												</ol>
-												<div class="row">
-													<div class="col-sm-4">
-														<div class="panel panel-primary">
-															<div class="panel-heading">
-																<div class="row">
-																	<div class="col-xs-3">
-																		<i class="fa fa-users fa-5x"></i>
-																	</div>
-																	<div class="col-xs-9">
-																		<div class="pull-right" style="font-size: 45px">10</div>
-																		<div class="clearfix"></div>
-																		<div class="pull-right">All Students</div>
-																	</div>
-																</div>
-															</div>
-															<a href="">
-																<div class="panel-footer">
-																	<span>All Students</span>
-																	<span class="pull-right">
-																		<i class="fas fa-arrow-circle-right"></i>
-																	</span>
-																	<div class="clearfix"></div>
-																</div>
-															</a>
-														</div>
-													</div>
-													<div class="col-sm-4">
-														<div class="panel panel-primary">
-															<div class="panel-heading">
-																<div class="row">
-																	<div class="col-xs-3">
-																		<i class="fa fa-users fa-5x"></i>
-																	</div>
-																	<div class="col-xs-9">
-																		<div class="pull-right" style="font-size: 45px">10</div>
-																		<div class="clearfix"></div>
-																		<div class="pull-right">All Students</div>
-																	</div>
-																</div>
-															</div>
-															<a href="">
-																<div class="panel-footer">
-																	<span>All Students</span>
-																	<span class="pull-right">
-																		<i class="fas fa-arrow-circle-right"></i>
-																	</span>
-																	<div class="clearfix"></div>
-																</div>
-															</a>
-														</div>
-													</div>
-													<div class="col-sm-4">
-														<div class="panel panel-primary">
-															<div class="panel-heading">
-																<div class="row">
-																	<div class="col-xs-3">
-																		<i class="fa fa-users fa-5x"></i>
-																	</div>
-																	<div class="col-xs-9">
-																		<div class="pull-right" style="font-size: 45px">10</div>
-																		<div class="clearfix"></div>
-																		<div class="pull-right">All Students</div>
-																	</div>
-																</div>
-															</div>
-															<a href="">
-																<div class="panel-footer">
-																	<span>All Students</span>
-																	<span class="pull-right">
-																		<i class="fas fa-arrow-circle-right"></i>
-																	</span>
-																	<div class="clearfix"></div>
-																</div>
-															</a>
-														</div>
-													</div>
-												</div>
+												
+												<?php
 
-                                               <hr>
+												
+                                                if(isset($_GET['page'])){
+													$page = $_GET['page'].'.php';
+												} else {
+													$page = "dashboard.php";
+												}
 
-											   <h3>New Students</h3>
+												 
+												
+												
+												if(file_exists($page)){
+													require_once $page;
+												} else {
+													echo '<h1>File Not Found</h1>';
+												}
+												   
 
-											   <div class="table-responsive">
-
-											   <table id="data" class="table table-hover table-bordered table-striped">
-											      <thead>
-												     <tr>
-													 <th>ID</th>
-													 <th>Name</th>
-													 <th>Roll</th>
-													 <th>City</th>
-													 <th>Contact</th>
-													 <th>Photo</th>
-													 </tr>
-												  </thead>
-												  <tbody>
-												    <tr>
-												       <td>1</td>
-													   <td>Faizul Islam</td>
-													   <td>1810021</td>
-													   <td>Rajshahi</td>
-													   <td>01715577668</td>
-													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
-												    </tr>
-													<tr>
-												       <td>1</td>
-													   <td>Faizul Islam</td>
-													   <td>1810021</td>
-													   <td>Rajshahi</td>
-													   <td>01715577668</td>
-													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
-												    </tr>
-													<tr>
-												       <td>1</td>
-													   <td>Faizul Islam</td>
-													   <td>1810021</td>
-													   <td>Rajshahi</td>
-													   <td>01715577668</td>
-													   <td><img style="width: 100px;" src="images/faizulece18.jpg" alt=""></td>
-												    </tr>
-												  </tbody>
-											   </table>
-
-											  </div>
-
+												?>
+                                               
 											</div>
 										</div>
 									</div>
