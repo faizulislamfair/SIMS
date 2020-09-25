de7d61976a1b9ebf01627f9dbe3e592f490ab838
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2020-09-25 13:09:39 +0600

    Fountain lmao

--- admin/user-profile.php
+++ admin/user-profile.php
@@ -3,39 +3,49 @@
     <li><a href="index.php?page=dashboard"><i class="fas fa-tachometer-alt"></i> Dashboard</a></li>
     <li class="active"><i class="fa fa-user"></i> Profile</li>
 </ol>
+
+<?php
+
+$session_user = $_SESSION['user_login'];
+
+$user_data = mysqli_query($link, "SELECT * FROM `users` WHERE `username` = '$session_user'");
+$user_row = mysqli_fetch_assoc($user_data);
+
+?>
+
 <div class="row">
   <div class="col-sm-6">
     <table class="table table-bordered">
       <tr>
         <td>User ID</td>
-        <td>101</td>
+        <td><?= $user_row['id']; ?></td>
       </tr>
       <tr>
         <td>Name</td>
-        <td>S.M. Faizul Islam Fair</td>
+        <td><?=  ucwords($user_row['name']); ?></td>
       </tr>
       <tr>
         <td>Username</td>
-        <td>faizulece18</td>
+        <td><?= $user_row['username']; ?></td>
       </tr>
       <tr>
         <td>Email</td>
-        <td>faizulislamfair@gmail.com</td>
+        <td><?= $user_row['email']; ?></td>
       </tr>
       <tr>
         <td>Status</td>
-        <td>Active</td>
+        <td><?= ucwords($user_row['status']); ?></td>
       </tr>
       <tr>
         <td>Signup Date</td>
-        <td>24-09</td>
+        <td><?= $user_row['datetime']; ?></td>
       </tr>
     </table>
     <a href="" class="btn btn-sm btn-info pull-right">Edit Profile</a>
   </div>
   <div class="col-sm-6">
     <a href="">
-      <img style="width:150px; height=250px;" class="img-thumbnail img-sm" src="images/faizulece18.jpg" alt="Thumbnail">
+      <img style="width:200px; height=250px;" class="img-thumbnail img-sm" src="images/<?= $user_row['photo'] ?>" alt="Thumbnail">
     </a>
     <br> <br>
     <form action="">
